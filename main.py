80d1425399b34a984dd9193cf256f77fe1f103a7
ZiadFellahIdrissi <ziad.fe1999@gmail.com>
2021-01-25 03:39:50 +0100

    trying some more

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from pyswip import Prolog
-from tkinter import Tk,Label
+from tkinter import Tk,Label,Entry,Button
+
+def get_it():
+    but="parent("+my_text.get()+",X)"
+    for i in p.query(but):
+        Label(root,text=i["X"]).pack()
+
 
 root = Tk()
 root.geometry("500x500")
@@ -7,8 +13,11 @@ root.geometry("500x500")
 p=Prolog()
 p.consult("C:/Users/fella/Desktop/BD2C/IA/prolog/test.pl")
 
-for i in p.query("parent(sara,X)"):
-    Label(root,text=i["X"]).pack()
+# input
+my_text = Entry(root,font=("Helvetica", 16) )
+my_text.pack(pady="15", padx="10")
 
+# button valider
+Button(root,text="D'aaaaccord" ,command=get_it).pack()
 
 root.mainloop()
