c86bfa0644d0d0556452bce4777b8552b1abfd5d
Yahya Khalid <63174192+clevernessisamyth@users.noreply.github.com>
2021-01-28 16:22:56 +0100

    l'ajout des conseils

--- comed.pl
+++ comed.pl
@@ -16,32 +16,56 @@ covid :- verifier(fievre),
 		 verifier('perdu du gout'),
 		 verifier('congestion nasale'),
 		 verifier('mal de gorge'),
-		 verifier(diarrhée),
+		 verifier('diarrhée'),
 		 verifier('maux de tête'),
-		 verifier('douleurs musculaires').
+		 verifier('douleurs musculaires'),
+		 writeln("Vos symptômes nous dites que vous avez la COVID-19."),
+		 writeln("On vous conseille de visiter votre institut médicale locale le plutôt possible.").
 covid :- verifier(fievre),
 		 verifier(toux),
 		 verifier('des difficultés respiratoires'),
 		 verifier('la fatigue'),
 		 verifier('perdu d\'odorat'),
-		 verifier('perdu du gout').
-covid :- verifier('perdu d\'odorat'),
-		 verifier('perdu du gout').
+		 verifier('perdu du gout'),
+		 writeln("Il est forte probable que vous avez la COVID-19."),
+		 writeln("On vous conseille de vous vous confinez chez vous pendant 14 jours,"),
+		 writeln("et prendre des medicaments.").
+covid :- verifier(fievre),
+		 verifier('perdu d\'odorat'),
+		 verifier('perdu du gout'),
+		 writeln("Il est probable que vous avez la COVID-19."),
+		 writeln("On vous conseille de vous vous confinez chez vous pendant 14 jours."),
+		 writeln("vous vous lavez les mains régulièrement pour ne pas transmettre le virus aux autres.").
 grippe :- verifier(fievre),
 		  verifier(toux),
 		  verifier('congestion nasale'),
 		  verifier('mal de gorge'),
 		  verifier('des difficultés respiratoires'),
 		  verifier('maux de tête'),
-		  verifier('douleurs musculaires').
+		  verifier('douleurs musculaires'),
+		  writeln("On vous conseille de vous vous reposez,"),
+		  writeln("bien buvez de l'eau,"),
+		  writeln("prenez un médicament contre la fièvre (nous vous recommandons le Paracétamol),"),
+		  writeln("vous vous lavez les mains régulièrement et ne prenez pas d'antibiotiques.").
 angine :- verifier(fievre),
 		  verifier('mal de gorge'),
 		  verifier('maux de tête'),
-		  verifier('amygdales rouges').
+		  verifier('amygdales rouges'),
+		  writeln("On vous conseille que vous consommez de la vitamine C,"),
+		  writeln("buvez beaucoup d'eau"),
+		  writeln("evitez les climatiseurs et radiateurs trop chauds,"),
+		  writeln("arrêtez de fumer si vous êtes fumeux,"),
+		  writeln("mangez du miel et dormez suffisament.").
 rhume :- verifier(fievre),
 		 verifier(toux),
 		 verifier('congestion nasale'),
-		 verifier('mal de gorge').
+		 verifier('mal de gorge'),
+		 writeln("On vous conseille que vous utiliez du sérum physiologique pour laver le nez,"),
+		 writeln("mangez du miel,"),
+		 writeln("stoppez l’écoulement du nez avec l’homéopathie"),
+		 writeln("apaisez les irritations nasales avec une pommade à base de vaseline,"),
+		 writeln("désinfecter le nez avec de gouttes nasales antiseptiques,"),
+		 writeln("prenez un médicament contre la fièvre (nous vous recommandons le Paracétamol).").
 
 verifier(Symptome) :- ( yes(Symptome) -> true ; (no(Symptome) -> fail ; demander(Symptome)) ).
 
